df48272f37ef156f5dd23b052f74f12935dae127
walid yusof <walid.yusof@yahoo.co.uk>
2020-05-12 16:28:22 +0100

    t

---
+++ src/test/java/com/HospitalV2/HospitalV2/HospitalV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HospitalV2.HospitalV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
